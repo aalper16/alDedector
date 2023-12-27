009e5539119a9387a20d36be3b570dbf1681a11e
Alper <135256391+aalper16@users.noreply.github.com>
2023-12-27 20:05:20 +0300

    fix: from wget to curl

--- download_YOLOv3.py
+++ download_YOLOv3.py
@@ -1,2 +1,2 @@
 import os 
-os.system('!wget https://github.com/OlafenwaMoses/ImageAI/releases/download/3.0.0-pretrained/yolov3.pt')
+os.system('curl https://github.com/OlafenwaMoses/ImageAI/releases/download/3.0.0-pretrained/yolov3.pt')
